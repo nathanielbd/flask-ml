e4facabf6eca43f292eb5c666a50034e2f1ec5cb
Nathaniel Budijono <nathanielbd@gmail.com>
2020-03-06 17:40:15 -0600

    templates, mock model, no template

--- app/app.py
+++ app/app.py
@@ -1,10 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for
+from forms import *
+from model import *
 
 app = Flask(__name__)
 
 @app.route('/')
 def landing():
-    return render_template("templates/landing.html")
+    title = 'Title'
+    form = ParametersForm()
+    form_name = 'Form name'
+    if form.validate_on_submit():
+        return redirect(url_for('results', data=form.param))
+    return render_template('templates/landing.html', title = title, form = form, form_name = form_name)
+
+@app.route('/results')
+def results():
+    data = request.args.get("data")
+    output = model(data)
+    return render_template('templates/results.html', output = output)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
---
+++ app/forms.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, SubmitField
+from wtforms.validators import DataRequired, Length
+
+class ParametersForm(FlaskForm):
+    param = StringField('Parameter_name', [DataRequired()])
+    submit = SubmitField('Submit')
+
+class ProceedForm(FlaskFrom):
+    submit = SubmitField('Go')
